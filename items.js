9703f92597b3f7a767fb402263b522f585c1afab
Bagerca <bagrationbebia@gmail.com>
2026-02-04 00:07:17 +0300

    Update items.js

--- items.js
+++ items.js
@@ -1,10 +1,23 @@
 const resources = [
-    { name: "Алмазный блок", amount: 64, id: "Diamond_Block" },
-    { name: "Незеритовый слиток", amount: 12, id: "Netherite_Ingot" },
-    { name: "Динамит (TNT)", amount: 128, id: "TNT" },
-    { name: "Золотое зачарованное яблоко", amount: 5, id: "Enchanted_Golden_Apple" },
-    { name: "Элитры (починка/проч 3)", amount: 2, id: "Elytra" },
-    { name: "Маяк", amount: 1, id: "Beacon" },
-    { name: "Шалкер бокс (черный)", amount: 10, id: "Black_Shulker_Box" },
-    // Копируй строчку выше и меняй данные
+    { name: "Стекло", amount: "192 (3 стака)", id: "Glass" },
+    { name: "Книжная полка", amount: 26, id: "Bookshelf" },
+    { name: "Полированный базальт", amount: 8, id: "Polished_Basalt" },
+    { name: "Тёмный дуб (бревно)", amount: "192 (3 стака)", id: "Dark_Oak_Log" },
+    { name: "Гранит", amount: "128 (2 стака)", id: "Granite" },
+    { name: "Рамка", amount: 31, id: "Item_Frame" }, // 26 + 5
+    { name: "Песчаник", amount: 64, id: "Sandstone" },
+    
+    // Животные (Яйца призыва)
+    { name: "Яйцо призыва: Корова", amount: 4, id: "Cow_Spawn_Egg" },
+    { name: "Яйцо призыва: Курица", amount: 4, id: "Chicken_Spawn_Egg" },
+    { name: "Яйцо призыва: Свинья", amount: 4, id: "Pig_Spawn_Egg" },
+    { name: "Яйцо призыва: Мул", amount: 1, id: "Mule_Spawn_Egg" },
+    { name: "Яйцо призыва: Лошадь", amount: 1, id: "Horse_Spawn_Egg" },
+    
+    { name: "Седло", amount: 1, id: "Saddle" },
+    { name: "Алмазная конская броня", amount: 1, id: "Diamond_Horse_Armor" },
+    { name: "Диорит", amount: 121, id: "Diorite" },
+    { name: "Песок", amount: "Дохуя (Много)", id: "Sand" },
+    { name: "Железный слиток", amount: 4, id: "Iron_Ingot" },
+    { name: "Слизь", amount: "Неизвестно", id: "Slimeball" },
 ];
