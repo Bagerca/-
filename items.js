f55de2b65a47448fb05c432a41886b2e387eb149
Bagerca <bagrationbebia@gmail.com>
2026-02-03 23:51:21 +0300

    Add resources array with item details

---
+++ items.js
@@ -0,0 +1,10 @@
+const resources = [
+    { name: "Алмазный блок", amount: 64, id: "Diamond_Block" },
+    { name: "Незеритовый слиток", amount: 12, id: "Netherite_Ingot" },
+    { name: "Динамит (TNT)", amount: 128, id: "TNT" },
+    { name: "Золотое зачарованное яблоко", amount: 5, id: "Enchanted_Golden_Apple" },
+    { name: "Элитры (починка/проч 3)", amount: 2, id: "Elytra" },
+    { name: "Маяк", amount: 1, id: "Beacon" },
+    { name: "Шалкер бокс (черный)", amount: 10, id: "Black_Shulker_Box" },
+    // Копируй строчку выше и меняй данные
+];
