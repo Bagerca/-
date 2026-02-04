c0647db5893ca9e9b008cbafd8a34dd03fed64d1
Bagerca <bagrationbebia@gmail.com>
2026-02-04 16:55:37 +0300

    Update items.js

--- items.js
+++ items.js
@@ -1,25 +1,29 @@
 const resources = [
-    { name: "Стекло", amount: "192 (3 стака)", id: "Glass" },
-    { name: "Книжная полка", amount: 26, id: "Bookshelf" },
-    { name: "Полированный базальт", amount: 8, id: "Polished_Basalt" },
-    { name: "Тёмный дуб (бревно)", amount: "192 (3 стака)", id: "Dark_Oak_Log" },
-    { name: "Гранит", amount: "128 (2 стака)", id: "Granite" },
-    { name: "Рамка", amount: 31, id: "Item_Frame" },
-    { name: "Песчаник", amount: 64, id: "Sandstone" },
+    { name: "Стекло", amount: "192 (3 стака)", id: "Glass", rarity: "common" },
+    { name: "Книжная полка", amount: 26, id: "Bookshelf", rarity: "common" },
+    { name: "Полированный базальт", amount: 8, id: "Polished_Basalt", rarity: "common" },
+    { name: "Тёмный дуб (бревно)", amount: "192 (3 стака)", id: "Dark_Oak_Log", rarity: "common" },
+    { name: "Гранит", amount: "128 (2 стака)", id: "Granite", rarity: "common" },
+    { name: "Рамка", amount: 31, id: "Item_Frame", rarity: "common" },
+    { name: "Песчаник", amount: 64, id: "Sandstone", rarity: "common" },
     
     // Животные
-    { name: "Яйцо призыва: Корова", amount: 4, id: "Cow_Spawn_Egg" },
-    { name: "Яйцо призыва: Курица", amount: 4, id: "Chicken_Spawn_Egg" },
-    { name: "Яйцо призыва: Свинья", amount: 4, id: "Pig_Spawn_Egg" },
-    { name: "Яйцо призыва: Мул", amount: 1, id: "Mule_Spawn_Egg" },
-    { name: "Яйцо призыва: Лошадь", amount: 1, id: "Horse_Spawn_Egg" },
+    { name: "Яйцо призыва: Корова", amount: 4, id: "Cow_Spawn_Egg", rarity: "common" },
+    { name: "Яйцо призыва: Курица", amount: 4, id: "Chicken_Spawn_Egg", rarity: "common" },
+    { name: "Яйцо призыва: Свинья", amount: 4, id: "Pig_Spawn_Egg", rarity: "common" },
+    { name: "Яйцо призыва: Мул", amount: 1, id: "Mule_Spawn_Egg", rarity: "common" },
+    { name: "Яйцо призыва: Лошадь", amount: 1, id: "Horse_Spawn_Egg", rarity: "common" },
     
-    { name: "Седло", amount: 1, id: "Saddle" },
-    // ЗАЧАРОВАННЫЙ ПРЕДМЕТ
-    { name: "Алмазная конская броня", amount: 1, id: "Diamond_Horse_Armor", enchanted: true },
+    { name: "Седло", amount: 1, id: "Saddle", rarity: "common" },
     
-    { name: "Диорит", amount: 121, id: "Diorite" },
-    { name: "Песок", amount: "Дохуя (Много)", id: "Sand" },
-    { name: "Железный слиток", amount: 4, id: "Iron_Ingot" },
-    { name: "Слизь", amount: "Неизвестно", id: "Slimeball" },
+    // РЕДКИЕ ВЕЩИ (Голубой цвет)
+    { name: "Алмазная конская броня", amount: 1, id: "Diamond_Horse_Armor", enchanted: true, rarity: "rare" },
+    
+    // Можно добавить Маяк, если он есть, как редкий
+    // { name: "Маяк", amount: 1, id: "Beacon", rarity: "rare" },
+
+    { name: "Диорит", amount: 121, id: "Diorite", rarity: "common" },
+    { name: "Песок", amount: "Дохуя (Много)", id: "Sand", rarity: "common" },
+    { name: "Железный слиток", amount: 4, id: "Iron_Ingot", rarity: "common" },
+    { name: "Слизь", amount: "Неизвестно", id: "Slimeball", rarity: "common" },
 ];
